6472f21a00f758ad7f78d6029b939a557ac09713
anverma007 <56126665+anverma007@users.noreply.github.com>
2019-11-27 23:15:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0d03add87774b12c5"
   instance_type = "t2.micro"
-  key_name      = "anv"
+  key_name      = "qwerty-key"
 
   tags = {
-    Name = "anverma"
+    Name = "anverma-007"
   }
 }
